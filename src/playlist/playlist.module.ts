1606093cd5529145dbbea746de8fb1b41978d0df
Leonardorossato <leonardorossato21@gmail.com>
2022-11-27 10:12:27 -0300

    import

--- src/playlist/playlist.module.ts
+++ src/playlist/playlist.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PlaylistService } from './playlist.service';
 import { PlaylistController } from './playlist.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Playlist } from './entities/playlist.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Playlist])],
   controllers: [PlaylistController],
   providers: [PlaylistService]
 })
