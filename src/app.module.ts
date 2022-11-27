2c8e7fc927bc4d1facd81c8547886275b6ae0162
Leonardorossato <leonardorossato21@gmail.com>
2022-11-27 10:10:06 -0300

    config module and ormcconfig import

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,16 @@ import { UsersModule } from './users/users.module';
 import { PlaylistModule } from './playlist/playlist.module';
 import { SongsModule } from './songs/songs.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmAsyncConfig } from './config/ormconfig';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [UsersModule, PlaylistModule, SongsModule, AuthModule]
+  imports: [TypeOrmModule.forRootAsync(typeOrmAsyncConfig), 
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule, 
+    PlaylistModule, 
+    SongsModule,
+    AuthModule
+  ]
 })
 export class AppModule {}
