ff5b14ee30e887e9eb072eeb442975822c2e78c2
Leonardorossato <leonardorossato21@gmail.com>
2022-11-27 11:24:46 -0300

    ok

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { UsersService } from './users.service';
 
+@ApiTags('Usuarios')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+  
+  @Get('/all')
+  async getAllUsers(){
+    return await this.usersService.all();
+  }
 }
