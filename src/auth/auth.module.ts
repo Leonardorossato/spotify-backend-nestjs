abfea4130327618a1c90ef154425c592c3c3c93d
Leonardorossato <leonardorossato21@gmail.com>
2022-11-27 10:37:27 -0300

    jwt confiuration in auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+	ConfigModule,
+    JwtModule.registerAsync({
+      useFactory: async () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: '1d', algorithm: 'HS384' },
+        verifyOptions: {
+          algorithms: ['HS384'],
+        },
+      }),
+      inject: [ConfigService],
+    })],
   controllers: [AuthController],
   providers: [AuthService]
 })
