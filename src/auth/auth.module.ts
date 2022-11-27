6c244a82688188bf89a8f8244140e69ac14c42c1
Leonardorossato <leonardorossato21@gmail.com>
2022-11-27 11:24:51 -0300

    ok

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,17 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from '../users/users.module';
+import { CheckPassword } from './middleware/check.pass.mid';
+import { LocalAuthGuard } from './guards/local.guard';
+import { JwtAuthGuard } from './guards/jwt.guard';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { LocalStrategy } from './strategy/local.strategy';
 
 @Module({
   imports: [
-	ConfigModule,
+    UsersModule,
+	  ConfigModule,
     JwtModule.registerAsync({
       useFactory: async () => ({
         secret: process.env.JWT_SECRET,
@@ -18,6 +25,6 @@ import { JwtModule } from '@nestjs/jwt';
       inject: [ConfigService],
     })],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, CheckPassword, LocalAuthGuard, JwtAuthGuard, JwtStrategy, LocalStrategy]
 })
 export class AuthModule {}
