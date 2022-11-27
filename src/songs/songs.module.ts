0763e08ff16d9f6ad160a38baa081c73fe44783e
Leonardorossato <leonardorossato21@gmail.com>
2022-11-27 10:12:31 -0300

    import

--- src/songs/songs.module.ts
+++ src/songs/songs.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SongsService } from './songs.service';
 import { SongsController } from './songs.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Songs } from './entities/songs.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Songs])],
   controllers: [SongsController],
   providers: [SongsService]
 })
