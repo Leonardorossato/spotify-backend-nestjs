c18e683f6e5cf6bf2f0d89a38c9905f4754d6fe5
Leonardorossato <leonardorossato21@gmail.com>
2022-11-27 09:20:14 -0300

    imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { PlaylistModule } from './playlist/playlist.module';
+import { SongsModule } from './songs/songs.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: []
+  imports: [UsersModule, PlaylistModule, SongsModule, AuthModule]
 })
 export class AppModule {}
