705867c5f57f95383a8474e32ec6667d3eded9d5
Leonardorossato <leonardorossato21@gmail.com>
2022-11-27 10:22:14 -0300

    services to work to users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FindOneOptions, Repository } from 'typeorm';
+import { CreateUserDTO } from './dto/create.user.dto';
+import { Users } from './entities/users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(Users) private usersRepository: Repository<Users>){}
+
+    async all(): Promise<Users[]>{
+        try {
+            const users = await this.usersRepository.find()
+            return users
+        } catch (error) {
+            throw new HttpException('Users not found', HttpStatus.NOT_FOUND)
+        }
+    }
+
+    async findOne(where: FindOneOptions<Users>): Promise<Users> {
+        const user = this.usersRepository.findOne(where);
+    
+        if (!user) {
+          throw new NotFoundException(
+            `There isn't any user with identifier: ${where}`,
+          );
+        }
+    
+        return user;
+    }
+
+    async findOneByEmail(dto) {
+        try {
+          const user = await this.usersRepository.findOneBy({ email: dto.email });
+          return user;
+        } catch (error) {
+          throw new NotFoundException('Email não encontrado');
+        }
+    }
+
+    async getByEmail(dto: CreateUserDTO) {
+        try {
+          const user = await this.usersRepository.findOneBy({ email: dto.email });
+          if (user) {
+            return user;
+          }
+          throw new HttpException(
+            'User with this email does not exist',
+            HttpStatus.BAD_REQUEST,
+          );
+        } catch (error) {
+          throw new HttpException('Email not found', HttpStatus.NOT_FOUND);
+        }
+    }
+
+    async create(dto: CreateUserDTO) {
+        try {
+          const user = await this.usersRepository.save({
+            ...dto,
+          });
+          return user;
+        } catch (error) {
+          throw new NotFoundException('Erro to create a new User');
+        }
+    }
+}
