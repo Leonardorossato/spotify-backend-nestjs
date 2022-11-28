9b8d01178518113c6c9ff9c031cbacb7377885b1
Leonardorossato <leonardorossato21@gmail.com>
2022-11-28 20:16:40 -0300

    controller to route playlist

--- src/playlist/playlist.controller.ts
+++ src/playlist/playlist.controller.ts
@@ -1,7 +1,8 @@
 import { Body, Controller, Post, Req } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
 import { CreatePlaylistDTO } from './dto/create.playlist.dto';
 import { PlaylistService } from './playlist.service';
-
+@ApiBearerAuth()
 @Controller('playlist')
 export class PlaylistController {
   constructor(private readonly playlistService: PlaylistService) {}
