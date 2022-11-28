b30b28c6a4ae0081da727bc11869be2e08bd88f7
Leonardorossato <leonardorossato21@gmail.com>
2022-11-28 20:17:36 -0300

    ok

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateSongDTO } from './dto/create.song.dto';
 import { SongsService } from './songs.service';
-
+@ApiTags('Musicas')
 @Controller('songs')
 export class SongsController {
   constructor(private readonly songsService: SongsService) {}
+
+  @Get('/all')
+  async getAllSongs() {
+    return await this.songsService.all()
+  }
+
+  @Post('/create')
+  async createSong(dto: CreateSongDTO){
+    return await this.songsService.create(dto)
+  }
 }
