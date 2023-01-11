ce755a151b20f2797922d738001852d061b6c212
Leonardorossato <leonardorossato21@gmail.com>
2023-01-11 15:03:22 -0300

    update playlist.service.ts

--- src/playlist/playlist.service.ts
+++ src/playlist/playlist.service.ts
@@ -1,42 +1,45 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { UsersService } from 'src/users/users.service';
-import { Repository } from 'typeorm';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Users } from 'src/users/schema/users.schema';
 import { AddSongPlaylistDTO } from './dto/add.song.dto';
 import { CreatePlaylistDTO } from './dto/create.playlist.dto';
-import { Playlist } from './entities/playlist.entity';
+import { Playlist } from './schema/playlist.schema';
 
 @Injectable()
 export class PlaylistService {
   constructor(
-    @InjectRepository(Playlist)
-    private readonly playlistRepository: Repository<Playlist>,
-    private readonly userService: UsersService
+    @InjectModel(Playlist.name)
+    private readonly playlistModel: Model<Playlist>,
+
+    @InjectModel(Users.name)
+    private readonly usersModel: Model<Users>,
   ) {}
 
   async all(): Promise<Playlist[]> {
     try {
-      const playlist = await this.playlistRepository.find();
+      const playlist = await this.playlistModel.find();
       return playlist;
     } catch (error) {
       throw new HttpException('Playlist not found', HttpStatus.NOT_FOUND);
     }
   }
 
-  async create(dto: CreatePlaylistDTO, id: number) {
+  async create(dto: CreatePlaylistDTO, id: string) {
     try {
-      const user = await this.userService.findOneById(id);
+      const user = await this.usersModel.findById({ _id: id });
       if (!user) {
         throw new HttpException(
           'User with this id not found',
           HttpStatus.NOT_FOUND,
         );
       }
-      const playlist = await this.playlistRepository.save({
+      const playlist = await this.playlistModel.create({
         ...dto,
         songs: dto.songs,
         userId: dto.userId,
       });
+      await playlist.save();
       return playlist;
     } catch (error) {
       throw new HttpException(
@@ -46,10 +49,12 @@ export class PlaylistService {
     }
   }
 
-  async addSong(id: number, dto: AddSongPlaylistDTO, req) {
+  async addSong(id: string, dto: AddSongPlaylistDTO, req) {
     try {
-      const user = await this.userService.findOneById(id);
-      const playlist = await this.playlistRepository.findOneBy({id: dto.playlistId});
+      const user = await this.usersModel.findById({ _id: id });
+      const playlist = await this.playlistModel.findOne({
+        id: dto.playlistId,
+      });
       if (!user.id && playlist.id) {
         throw new HttpException(
           'User dont create to add song to this playlist',
@@ -57,24 +62,31 @@ export class PlaylistService {
         );
       }
       if (playlist.songs.indexOf(req.body.songId) === -1) {
-        playlist.songs.push(req.body.songsId)
+        playlist.songs.push(req.body.songsId);
       }
-      await this.playlistRepository.save(dto)
-      return playlist
+      await this.playlistModel.create(dto);
+      await playlist.save();
+      return playlist;
     } catch (error) {
-      throw new HttpException('Erro to add song to playlist', HttpStatus.BAD_REQUEST)
+      throw new HttpException(
+        'Erro to add song to playlist',
+        HttpStatus.BAD_REQUEST,
+      );
     }
   }
 
-  async findOneById(id: number){
+  async findOneById(id: string) {
     try {
-      const playlist = await this.playlistRepository.findOneBy({id: id})
+      const playlist = await this.playlistModel.findById({ _id: id });
       if (!playlist) {
-        throw new HttpException('Playlist with id no found', HttpStatus.NOT_FOUND)
+        throw new HttpException(
+          'Playlist with id no found',
+          HttpStatus.NOT_FOUND,
+        );
       }
       return playlist;
     } catch (error) {
-      throw new HttpException('Error', HttpStatus.BAD_REQUEST)
+      throw new HttpException('Error', HttpStatus.BAD_REQUEST);
     }
   }
 }
