66353c815c9963843c178f87c07e54839474ecd4
Leonardorossato <leonardorossato21@gmail.com>
2023-01-11 18:03:51 -0300

    update playlist.controller.ts

--- src/playlist/playlist.controller.ts
+++ src/playlist/playlist.controller.ts
@@ -1,9 +1,11 @@
 import { Body, Controller, Param, Post, Put, Req } from '@nestjs/common';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { AddSongPlaylistDTO } from './dto/add.song.dto';
 import { CreatePlaylistDTO } from './dto/create.playlist.dto';
 import { PlaylistService } from './playlist.service';
+
 @ApiBearerAuth()
+@ApiTags('Playlist')
 @Controller('playlist')
 export class PlaylistController {
   constructor(private readonly playlistService: PlaylistService) {}
