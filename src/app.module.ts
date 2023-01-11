778e694bc69c24e5b332f79b4bf253e70b70592a
Leonardorossato <leonardorossato21@gmail.com>
2023-01-11 13:15:43 -0300

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { PlaylistModule } from './playlist/playlist.module';
 import { SongsModule } from './songs/songs.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmAsyncConfig } from './config/ormconfig';
+import { typeOrmAsyncConfig } from './config/mongo.config';
 import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
