0768380b694ae21ad518e6b8538cf90eca1e305b
Leonardorossato <leonardorossato21@gmail.com>
2023-01-11 21:18:36 -0300

    update playlist.controller.ts and playlist.service.ts

--- src/playlist/playlist.controller.ts
+++ src/playlist/playlist.controller.ts
@@ -1,7 +1,18 @@
-import { Body, Controller, Param, Post, Put, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  Req,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { AddSongPlaylistDTO } from './dto/add.song.dto';
 import { CreatePlaylistDTO } from './dto/create.playlist.dto';
+import { UpdatePlaylistDTO } from './dto/update.platlist.dto';
 import { PlaylistService } from './playlist.service';
 
 @ApiBearerAuth()
@@ -9,6 +20,26 @@ import { PlaylistService } from './playlist.service';
 @Controller('playlist')
 export class PlaylistController {
   constructor(private readonly playlistService: PlaylistService) {}
+
+  @Get('/all')
+  async all() {
+    return await this.playlistService.all();
+  }
+
+  @Get('playlist/:search')
+  async searchPartitionPlaylist(
+    @Req() req,
+    @Query('page') page: number,
+    @Query('limit') limit: number,
+    @Query('sort') sort: number,
+  ) {
+    return await this.playlistService.searchPartitionPlaylist(
+      page,
+      limit,
+      sort,
+      req,
+    );
+  }
 
   @Post('/create')
   async create(@Param('id') id: string, @Body() dto: CreatePlaylistDTO) {
@@ -22,5 +53,19 @@ export class PlaylistController {
     @Req() req,
   ) {
     return await this.playlistService.addSong(id, dto, req);
+  }
+
+  @Put('/playlist/:id')
+  async updated(
+    @Param('id') id: string,
+    @Req() req,
+    @Body() dto: UpdatePlaylistDTO,
+  ) {
+    return await this.playlistService.update(id, req, dto);
+  }
+
+  @Delete('/playlist/:id')
+  async delete(@Param('id') id: string, @Req() req) {
+    return await this.playlistService.remove(id, req);
   }
 }
--- src/playlist/playlist.service.ts
+++ src/playlist/playlist.service.ts
@@ -4,6 +4,7 @@ import { Model } from 'mongoose';
 import { Users } from 'src/users/schema/users.schema';
 import { AddSongPlaylistDTO } from './dto/add.song.dto';
 import { CreatePlaylistDTO } from './dto/create.playlist.dto';
+import { UpdatePlaylistDTO } from './dto/update.platlist.dto';
 import { Playlist } from './schema/playlist.schema';
 
 @Injectable()
@@ -25,9 +26,33 @@ export class PlaylistService {
     }
   }
 
+  async searchPartitionPlaylist(req, page: number, limit: number, sort: number) {
+    try {
+      let { page, limit, sort, asc } = req;
+      if (!page) {
+        page = 1;
+      }
+      if (!limit) {
+        limit = 10;
+      }
+      const skip = (page - 1) * 10;
+      const playList = await this.playlistModel
+        .find()
+        .sort({ [sort]: -1 })
+        .skip(skip)
+        .limit(limit);
+      return { playList, page: page + 1, limit };
+    } catch (error) {
+      throw new HttpException(
+        'Error to search advacend playlist',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+  }
+
   async create(dto: CreatePlaylistDTO, id: string) {
     try {
-      const user = await this.usersModel.findOne({where: {id: id}});
+      const user = await this.usersModel.findOne({ where: { id: id } });
       if (!user) {
         throw new HttpException(
           'User with this id not found',
@@ -51,7 +76,7 @@ export class PlaylistService {
 
   async addSong(id: string, dto: AddSongPlaylistDTO, req) {
     try {
-      const user = await this.usersModel.findOne({ where: {id: id} });
+      const user = await this.usersModel.findOne({ where: { id: id } });
       const playlist = await this.playlistModel.findOne({
         id: dto.playlistId,
       });
@@ -71,6 +96,50 @@ export class PlaylistService {
       throw new HttpException(
         'Erro to add song to playlist',
         HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  async update(id: string, dto: UpdatePlaylistDTO, req) {
+    try {
+      const playlist = await this.playlistModel
+        .findById({ _id: id })
+        .select('userId');
+      if (!playlist)
+        throw new HttpException(
+          `Erro to find a playlist with this id: ${id}`,
+          HttpStatus.NOT_FOUND,
+        );
+      const user = await this.usersModel.findOne({ where: { id: id } });
+      await this.playlistModel.findByIdAndUpdate(user._id, { dto });
+      return { message: 'Playlist updated successfuly.' };
+    } catch (error) {
+      throw new HttpException(
+        'Error to update a playlist ',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async remove(id: string, req) {
+    try {
+      const playlist = await this.playlistModel.findOne({ where: { id: id } });
+      if (!playlist)
+        throw new HttpException(
+          `Erro to find a playlist with this id: ${id}`,
+          HttpStatus.NOT_FOUND,
+        );
+      const user = await this.usersModel.findOne({ where: { id: id } });
+      const index = user.playlist.indexOf(req.params.id);
+      user.playlist.splice(index, 1);
+
+      await user.save();
+      await playlist.remove();
+      return { message: 'Playlist deleted succesfully.' };
+    } catch (error) {
+      throw new HttpException(
+        'Error to remove a playlist: ',
+        HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
   }
