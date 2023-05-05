76f7152c428e9073b74200b92608b20a41fd101f
Leonardorossato21 <leonardorossato21@gmail.com>
2023-05-05 11:14:38 -0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Spotify Backend Nestjs Clone')
+    .setDescription('Endpoints da api do spotify clone')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
