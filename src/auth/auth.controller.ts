746c10f23c990d0055c477f47f1d17ca8d994e53
Leonardorossato21 <leonardorossato21@gmail.com>
2023-05-05 11:09:46 -0300

    update auth.controller.ts and auth.service.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthLoginDto } from './dto/auth.login.dto';
+import { Public } from 'nest-keycloak-connect';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
+@ApiTags('Autenticação')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Public()
+  @Post('/login')
+  async login(dto: AuthLoginDto) {
+    return await this.authService.login(dto);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import axios from 'axios';
+import { AuthLoginDto } from './dto/auth.login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor() {}
+
+  async login(dto: AuthLoginDto) {
+    try {
+      const login = await axios.post(
+        `${process.env.KEYCLOAK_URL}/realms/${process.env.KEYCLOAK_REALM}/protocol/openid-connect/token`,
+        {
+          ...dto,
+          grant_type: 'password',
+          client_id: `${process.env.KEYCLOAK_CLIENT_ID}`,
+          client_secret: `${process.env.KEYCLOAK_SECRET}`,
+          scope: 'openid',
+        },
+        {
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        },
+      );
+      return { access_token: login.data.access_token };
+    } catch (error) {
+      throw new HttpException('Credenciais invalidas', HttpStatus.BAD_REQUEST);
+    }
+  }
+}
