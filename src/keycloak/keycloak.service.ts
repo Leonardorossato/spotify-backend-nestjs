0e55a5ecfa592cdb005e64a836bb07c0cda7b94b
Leonardorossato21 <leonardorossato21@gmail.com>
2023-05-05 10:35:54 -0300

    feat: create keycloak.service.ts

---
+++ src/keycloak/keycloak.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import {
+  KeycloakConnectOptions,
+  KeycloakConnectOptionsFactory,
+  PolicyEnforcementMode,
+  TokenValidation,
+} from 'nest-keycloak-connect';
+
+@Injectable()
+export class KeycloakService implements KeycloakConnectOptionsFactory {
+  createKeycloakConnectOptions(): KeycloakConnectOptions {
+    return {
+      authServerUrl: process.env.KEYCLOAK_URL,
+      realm: process.env.KEYCLOAK_REALM,
+      clientId: process.env.KEYCLOAK_CLIENT_ID,
+      secret: process.env.KEYCLOAK_SECRET,
+      policyEnforcement: PolicyEnforcementMode.PERMISSIVE,
+      tokenValidation: TokenValidation.ONLINE,
+      'bearer-only': true,
+      public: true,
+    };
+  }
+}
