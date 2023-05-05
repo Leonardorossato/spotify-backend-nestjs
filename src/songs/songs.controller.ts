6cdcceed52a4ecf252662f22b438f4a1f729f769
Leonardorossato21 <leonardorossato21@gmail.com>
2023-05-05 15:31:09 -0300

    update songs.controller.ts

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -2,7 +2,9 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateSongDto } from './dto/create-song.dto';
 import { SongsService } from './songs.service';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { Roles } from 'nest-keycloak-connect';
+import { Public, Roles } from 'nest-keycloak-connect';
+import dotenv from 'dotenv';
+dotenv.config({ path: '/.env' });
 
 @Controller('songs')
 @ApiBearerAuth()
@@ -11,15 +13,16 @@ export class SongsController {
   constructor(private readonly songsService: SongsService) {}
 
   @Post('/create')
+  @Roles({
+    roles: [`realm: ${process.env.KEYCLOAK_CLIENT_ID}-muscian-create`],
+  })
   create(@Body() createSongDto: CreateSongDto) {
     return this.songsService.create(createSongDto);
   }
 
   @Get('/all')
   @Roles({
-    roles: [
-      `realm:${process.env.KEYCLOAK_CLIENT_ID}-${process.env.KEYCLOAK_CLIENT_MUSCIAN}-read`,
-    ],
+    roles: [`realm:${process.env.KEYCLOAK_CLIENT_ID}-muscian-read`],
   })
   async findAll() {
     return await this.songsService.findAll();
