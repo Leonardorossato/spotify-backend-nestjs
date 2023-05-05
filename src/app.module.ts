edc2999eb322d3b09875113cf77dafc813df68e7
Leonardorossato21 <leonardorossato21@gmail.com>
2023-05-05 10:24:45 -0300

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SongsModule } from './songs/songs.module';
+import { KeycloakModule } from './keycloak/keycloak.module';
 
 @Module({
-  imports: [SongsModule]
+  imports: [SongsModule, KeycloakModule]
 })
 export class AppModule {}
