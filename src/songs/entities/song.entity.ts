0cff99d4e1f327cbb804666ed5d23c7e88da3862
Leonardorossato21 <leonardorossato21@gmail.com>
2023-05-05 10:56:31 -0300

    update song.entity.ts

--- src/songs/entities/song.entity.ts
+++ src/songs/entities/song.entity.ts
@@ -1,1 +1,13 @@
-export class Song {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Song {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  duration: string;
+}
