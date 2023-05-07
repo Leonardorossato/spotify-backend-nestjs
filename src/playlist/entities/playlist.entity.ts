ac9315a505f4082ec5e059f7edb075a2362eef17
Leonardorossato <leonardorossato21@gmail.com>
2023-05-07 18:31:45 -0300

    update playlist.entity.ts

--- src/playlist/entities/playlist.entity.ts
+++ src/playlist/entities/playlist.entity.ts
@@ -1,1 +1,28 @@
-export class Playlist {}
+import { Song } from '@/songs/entities/song.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Playlist {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'varchar' })
+  name!: string;
+
+  @ManyToMany(() => Song, (song) => song.id)
+  @JoinColumn({ name: 'songId' })
+  song!: number[];
+
+  @Column({ nullable: false })
+  songId!: number;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+}
