29cb777895f09d44f13690b380223f52b335b203
Leonardorossato21 <leonardorossato21@gmail.com>
2023-05-08 09:22:07 -0300

    feat: create 1683546718041-playlist.ts

---
+++ src/migrations/1683546718041-playlist.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Playlist1683546718041 implements MigrationInterface {
+  name = 'Playlist1683546718041';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "playlist" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "songId" integer NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_538c2893e2024fabc7ae65ad142" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "playlist"`);
+  }
+}
