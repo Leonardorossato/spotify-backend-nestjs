0afb69bbc407f3f0bdb1d82d6c8f77bd69417156
Leonardorossato21 <leonardorossato21@gmail.com>
2023-05-08 10:39:37 -0300

    update playlist.service.ts

--- src/playlist/playlist.service.ts
+++ src/playlist/playlist.service.ts
@@ -17,10 +17,15 @@ export class PlaylistService {
 
   async create(dto: CreatePlaylistDto) {
     try {
-      const playlist = await this.playlistRepository.create(dto)
-      await this.playlistRepository.save(playlist)
-      return playlist
-    } catch (error) {}
+      const playlist = await this.playlistRepository.create(dto);
+      await this.playlistRepository.save(playlist);
+      return playlist;
+    } catch (error) {
+      throw new HttpException(
+        'Error creating playlist',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 
   async findAll() {
@@ -35,15 +40,62 @@ export class PlaylistService {
     }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} playlist`;
+  async findOne(id: number) {
+    try {
+      const playlist = await this.playlistRepository.findOneBy({ id: id });
+      if (!playlist) {
+        throw new HttpException(
+          `Error find a playlist with this id: ${id}`,
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      return playlist;
+    } catch (error) {
+      throw new HttpException(
+        `Error to find playlist with this id: ${id}`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
   }
 
-  update(id: number, updatePlaylistDto: UpdatePlaylistDto) {
-    return `This action updates a #${id} playlist`;
+  async update(id: number, dto: UpdatePlaylistDto) {
+    try {
+      const playlist = await this.playlistRepository.findOneBy({ id: id });
+      const song = await this.songRepository.findOneBy({
+        id: playlist?.songId,
+      });
+      if (song?.id) {
+        throw new HttpException(
+          `Error to update playlist with this song id: ${song.id}`,
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      await this.playlistRepository.update(id, dto);
+      return playlist;
+    } catch (error) {
+      throw new HttpException(
+        `Error to update playlist with this id: ${id}`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} playlist`;
+  async remove(id: number) {
+    try {
+      const playlist = await this.playlistRepository.findOneBy({ id: id });
+      if (!playlist) {
+        throw new HttpException(
+          'Error find a playlist with this id: ${id}',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      await this.playlistRepository.delete(playlist);
+      return { message: `Playlist deleted successfully` };
+    } catch (error) {
+      throw new HttpException(
+        `Error to remove playlist with this id: ${id}`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
